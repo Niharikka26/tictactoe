83607fc7631b9560f121063dd45901dcc54c78dc
Niharikka26 <nv26112000@gmail.com>
2021-08-31 16:03:53 +0530

    second commit

---
+++ tictactoe/src/components/Board.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <div>
+      <div><Square value={0}/>
+      <Square value={0} />
+      <Square value={0} />
+
+    </div>
+    <div><Square value={0}  /><Square value={0}  /><Square value={0}  />
+
+</div>
+<div><Square  value={0} /><Square value={0}  /><Square value={0}  />
+
+    </div>
+
+    </div>
+  )
+  }
+  
+
+export default Board  
